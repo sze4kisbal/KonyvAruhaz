7f59ef5e8a8fcfbe8e77d13a10751f307567d312
Kiss Bálint <sze4kisbal@vasvari.org>
2026-03-04 18:51:11 +0100

    gitignore javitas, build fajlok eltavolitva; Swagger + Konyvek API CRUD (GET/POST/PUT/DELETE) hozzaadva

---
+++ src/KonyvAruhaz.Web/Adat/AdatSeeder.cs
@@ -0,0 +1,85 @@
+﻿using KonyvAruhaz.Infrastruktura.Adat;
+using KonyvAruhaz.Doman.Entitasok;
+using Microsoft.EntityFrameworkCore;
+
+namespace KonyvAruhaz.Web.Adat;
+
+public static class AdatSeeder
+{
+    public static async Task SeedAsync(IServiceProvider szolgaltato)
+    {
+        using var scope = szolgaltato.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<KonyvAruhazDbContext>();
+
+        // Biztonság kedvéért: migrációk lefuttatása
+        await db.Database.MigrateAsync();
+
+        // 1) Kategóriák + 2) Könyvek
+        if (!await db.Kategoriak.AnyAsync())
+        {
+            var k1 = new Kategoria { Nev = "Regény" };
+            var k2 = new Kategoria { Nev = "Informatika" };
+
+            db.Kategoriak.AddRange(k1, k2);
+            await db.SaveChangesAsync();
+
+            db.Konyvek.AddRange(
+                new Konyv { Cim = "C# kezdőknek", Ar = 4990m, Aktiv = true, KategoriaId = k2.Id },
+                new Konyv { Cim = "SQL alapok", Ar = 3990m, Aktiv = true, KategoriaId = k2.Id },
+                new Konyv { Cim = "Egy jó regény", Ar = 2990m, Aktiv = true, KategoriaId = k1.Id }
+            );
+            await db.SaveChangesAsync();
+        }
+
+        // 3) Felhasználók (1 admin + 1 user)
+        if (!await db.Set<Felhasznalo>().AnyAsync())
+        {
+            var admin = new Felhasznalo
+            {
+                Email = "admin@teszt.hu",
+                JelszoHash = "admin123", // MOST még csak demo! később hash
+                Szerepkor = "Admin",
+                Aktiv = true
+            };
+
+            var user = new Felhasznalo
+            {
+                Email = "jani@teszt.hu",
+                JelszoHash = "jani123", // demo
+                Szerepkor = "Felhasznalo",
+                Aktiv = true
+            };
+
+            db.AddRange(admin, user);
+            await db.SaveChangesAsync();
+        }
+
+        // 4) 1 példa rendelés (ha még nincs)
+        if (!await db.Set<Rendeles>().AnyAsync())
+        {
+            var user = await db.Set<Felhasznalo>()
+                .FirstAsync(x => x.Email == "jani@teszt.hu");
+
+            var konyv = await db.Konyvek.FirstAsync();
+
+            var rendeles = new Rendeles
+            {
+                FelhasznaloId = user.Id,
+                LetrehozasIdeje = DateTime.UtcNow,
+                Allapot = "Uj",
+                Tetelek = new List<RendelesTetel>
+                {
+                    new RendelesTetel
+                    {
+                        KonyvId = konyv.Id,
+                        Mennyiseg = 2,
+                        EgysegAr = konyv.Ar
+                    }
+                }
+            };
+
+            db.Add(rendeles);
+            await db.SaveChangesAsync();
+        }
+    }
+}
--- src/KonyvAruhaz.Web/Program.cs
+++ src/KonyvAruhaz.Web/Program.cs
@@ -1,61 +1,52 @@
 using KonyvAruhaz.Infrastruktura.Adat;
+using KonyvAruhaz.Web.Adat;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using System.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// MVC + API
 builder.Services.AddControllersWithViews();
-//Össze kötés
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// DB
 builder.Services.AddDbContext<KonyvAruhazDbContext>(opcio =>
 {
     opcio.UseSqlServer(builder.Configuration.GetConnectionString("AlapAdatbazis"));
 });
 
 var app = builder.Build();
-//Sajat teszt
-using (var scope = app.Services.CreateScope())
+
+// Seed + migráció (csak EZ kell, a "Sajat teszt" blokkot töröljük)
+await AdatSeeder.SeedAsync(app.Services);
+
+// Swagger UI csak Developmentben
+if (app.Environment.IsDevelopment())
 {
-    var db = scope.ServiceProvider.GetRequiredService<KonyvAruhaz.Infrastruktura.Adat.KonyvAruhazDbContext>();
-
-    if (!db.Kategoriak.Any())
-    {
-        var kategoria = new KonyvAruhaz.Doman.Entitasok.Kategoria { Nev = "Regény" };
-        db.Kategoriak.Add(kategoria);
-
-        db.Konyvek.Add(new KonyvAruhaz.Doman.Entitasok.Konyv
-        {
-            Cim = "Teszt könyv",
-            Ar = 3990,
-            Kategoria = kategoria,
-            Aktiv = true
-        });
-
-        db.SaveChanges();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
+// MVC route
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
+// API route-ok (ApiController-ekhez)
+app.MapControllers();
 
 app.Run();
